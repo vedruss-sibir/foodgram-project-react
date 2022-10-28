d3f72bd051c490d731e2b8d83e51e38fe76ed46a
Андрей Стрельников <andre33_3@rambler.ru>
2022-10-28 13:09:36 +0600

    user

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.TokenAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
 
